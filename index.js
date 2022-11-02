30236e784c0eb0d9183ffc4815ac1fbb84a9fd5d
John Otto <jotto88@gmail.com>
2022-11-02 18:32:46 -0400

    commit tree 87f2d229ef76b87617fc9c52d9de4a380ff25ce4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
+}
+function distanceFromHqInFeet(someValue) {
+  return (distanceFromHqInBlocks(someValue) * 264);
+}
+function distanceTravelledInFeet(start, destination){
+  return (Math.abs(start - destination) * 264);
+}
+function calculatesFarePrice(start, destination){
+  let feet = total - 400
+  if (total < 400) {
+    return 0;
+  }
+  else if (total > 400 && total < 2000) {
+    return feet * 0.02;
+  }
+  else if (total > 2000 && total < 2500) {
+    return 25;
+  }
+  else {
+      return 'cannot travel that far'
+  }
+}
